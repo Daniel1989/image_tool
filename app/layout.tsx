afcc60ce72975d28b5945679158c85261ebb20fd
daniel <riyueniao2010@gmail.com>
2025-05-27 22:41:01 +0800

    feat: update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
-import  SpeedInsights  from "@vercel/speed-insights/astro"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
